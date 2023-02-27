e4e4685e4ab0459c85d353bf129f060c554d70b9
JinchengHeRyan <delteforce@outlook.com>
2023-02-27 14:41:42 -0500

    Project init commit

---
+++ server/src/test/java/edu/duke/ece651/teamX/server/AppTest.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.teamX.server;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void test_GetMessage() {
+      App a = new App();
+      assertEquals("Hello from the server for teamX", a.getMessage());
+    }
+}
+
---
+++ shared/src/main/java/edu/duke/ece651/teamX/shared/MyName.java
@@ -0,0 +1,7 @@
+package edu.duke.ece651.teamX.shared;
+
+public class MyName {
+  public static String getName(){
+    return "teamX";
+  }
+}
