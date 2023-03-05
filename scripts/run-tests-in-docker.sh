598c9308db95a81c13009993a03171fb6857efa6
JinchengHeRyan <delteforce@outlook.com>
2023-03-05 17:55:52 -0500

    Update test scripts

--- scripts/run-tests-in-docker.sh
+++ scripts/run-tests-in-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir coverage
-#docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
-docker run --rm -v `pwd`/coverage:/coverage-out  citest bash
+docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
+#docker run --rm -v `pwd`/coverage:/coverage-out  citest bash
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
-cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
-cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
-cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
+cp client/build/reports/clover/html/clover.xml /coverage-out/client_clover.xml || exit 1
+cp server/build/reports/clover/html/clover.xml /coverage-out/server_clover.xml || exit 1
+cp shared/build/reports/clover/html/clover.xml /coverage-out/shared_clover.xml || exit 1
 
