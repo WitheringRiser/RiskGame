36bf2b5c3fc57c745bd61ac665c140a8e6456122
JinchengHeRyan <delteforce@outlook.com>
2023-03-06 01:04:31 -0500

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
-cp client/build/reports/clover/html/* /coverage-out/client_clover.xml || exit 1
-cp server/build/reports/clover/html/* /coverage-out/server_clover.xml || exit 1
-cp shared/build/reports/clover/html/* /coverage-out/shared_clover.xml || exit 1
+cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
 
