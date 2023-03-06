0e1d399debc9692f0fc67e4b40e453528d2789fa
JinchengHeRyan <delteforce@outlook.com>
2023-03-06 01:26:22 -0500

    Remove generate report

--- scripts/run-tests-in-docker.sh
+++ scripts/run-tests-in-docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 mkdir coverage
 docker run --rm -v `pwd`/coverage:/coverage-out  citest scripts/test.sh
-#docker run --rm -v `pwd`/coverage:/coverage-out  citest bash
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,7 +3,7 @@
 ./gradlew build || exit 1
 ./gradlew cloverGenerateReport || exit 1
 scripts/coverage_summary.sh
-cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
-cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
-cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
+#cp -r client/build/reports/clover/html/* /coverage-out/ || exit 1
+#cp -r server/build/reports/clover/html/* /coverage-out/ || exit 1
+#cp -r shared/build/reports/clover/html/* /coverage-out/ || exit 1
 
