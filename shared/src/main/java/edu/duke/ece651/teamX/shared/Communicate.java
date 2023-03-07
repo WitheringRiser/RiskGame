f68ebf1308c7788c75ad220ac493f01855b20dc4
JinchengHeRyan <delteforce@outlook.com>
2023-03-07 13:52:58 -0500

    Add documentation in Communicate

--- shared/src/main/java/edu/duke/ece651/teamX/shared/Communicate.java
+++ shared/src/main/java/edu/duke/ece651/teamX/shared/Communicate.java
@@ -10,22 +10,52 @@ import java.net.Socket;
 
 public class Communicate {
 
+  /**
+   * Send object through socket
+   *
+   * @param socket
+   * @param obj
+   * @throws IOException
+   */
   public void sendObject(Socket socket, Object obj) throws IOException {
     OutputStream outputStream = socket.getOutputStream();
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
     objectOutputStream.writeObject(obj);
   }
 
+  /**
+   * Send map through socket
+   *
+   * @param socket
+   * @param map
+   * @throws IOException
+   */
   public void sendMap(Socket socket, Map map) throws IOException {
     sendObject(socket, map);
   }
 
+  /**
+   * Receive object through socket
+   *
+   * @param socket
+   * @return
+   * @throws IOException
+   * @throws ClassNotFoundException
+   */
   public Object receiveObject(Socket socket) throws IOException, ClassNotFoundException {
     InputStream inputStream = socket.getInputStream();
     ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
     return objectInputStream.readObject();
   }
 
+  /**
+   * Receive map through socket
+   *
+   * @param socket
+   * @return
+   * @throws IOException
+   * @throws ClassNotFoundException
+   */
   public Map receiveMap(Socket socket) throws IOException, ClassNotFoundException {
     return (Map) receiveObject(socket);
   }
