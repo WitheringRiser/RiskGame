4c0eb1f7085d63ae8533d5f55fa1100f29f2b056
lb439 <lb439@duke.edu>
2023-03-08 20:44:07 -0500

    Update Unit.java

--- shared/src/main/java/edu/duke/ece651/teamX/shared/Unit.java
+++ shared/src/main/java/edu/duke/ece651/teamX/shared/Unit.java
@@ -3,8 +3,5 @@ package edu.duke.ece651.teamX.shared;
 import java.io.Serializable;
 
 public abstract class Unit implements Serializable {
-    private int num;
-    public Unit(int num){
-        this.num = num;
-    }
+
 }
