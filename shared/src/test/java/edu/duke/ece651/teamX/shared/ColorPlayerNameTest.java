66a8250c5117bd1e9d6de49ed351f4ad7f206a4e
Xinmeng <xc139@duke.edu>
2023-03-10 00:39:23 -0500

    add PlayerName class

---
+++ shared/src/test/java/edu/duke/ece651/teamX/shared/ColorPlayerNameTest.java
@@ -0,0 +1,20 @@
+package edu.duke.ece651.teamX.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class ColorPlayerNameTest {
+  private void assert_name_equals(ColorPlayerName color_names, String... expect) {
+    for (String s : expect) {
+      assertEquals(s, color_names.getName());
+    }
+  }
+  @Test
+  public void test_getName() {
+    ColorPlayerName color_names = new ColorPlayerName();
+    assert_name_equals(
+        color_names, "Red", "Green", "Blue", "Yellow", "Orange", "Pink", "Purple");
+    assertThrows(IllegalArgumentException.class, () -> color_names.getName());
+  }
+}
