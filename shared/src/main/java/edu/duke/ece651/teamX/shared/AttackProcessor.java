e5b45d4517d13e86035c068980ea52764641b18f
Zhou An <anzhooou@163.com>
2023-03-14 19:45:21 -0400

    test broken

---
+++ shared/src/main/java/edu/duke/ece651/teamX/shared/AttackProcessor.java
@@ -0,0 +1,56 @@
+package edu.duke.ece651.teamX.shared;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class AttackProcessor {
+  private ArrayList<MultiAttack> allAttack;
+  private Map map;
+
+  AttackProcessor(ArrayList<AttackSender> _allAttack, Map _map) {
+    map = _map;
+    allAttack = getAttackList(_allAttack);
+    
+  }
+
+  public ArrayList<MultiAttack> getAttackList(ArrayList<AttackSender> _allAttack) {
+    HashMap<Territory, ArrayList<Attacker>> hashmap = getHashMap(_allAttack);
+    ArrayList<MultiAttack> allAttack = new ArrayList<MultiAttack>();
+    for (Territory defender : hashmap.keySet()) {
+      allAttack.add(new MultiAttack(hashmap.get(defender), defender, map));
+    }
+    return allAttack;
+  }
+
+  public HashMap<Territory, ArrayList<Attacker>> getHashMap(ArrayList<AttackSender> allAttack) {
+    HashMap<Territory, ArrayList<Attacker>> ans = new HashMap<Territory, ArrayList<Attacker>>();
+    for (int i = 0; i < allAttack.size(); ++i) {
+      AttackSender attack = allAttack.get(i);
+      Territory defender = attack.getDestination();
+      Territory enemy = attack.getSource();
+      int num = attack.getUnitsNum();
+
+      if (ans.containsKey(defender)) {
+        ans.get(defender).add(new Attacker(enemy, num, map.getOwner(enemy)));
+      } else {
+        ArrayList<Attacker> attackers = new ArrayList<Attacker>();
+        attackers.add(new Attacker(enemy, num, map.getOwner(enemy)));
+        ans.put(defender, attackers);
+      }
+    }
+    return ans;
+  }
+
+  public void resovleAllAttack() {
+    for (MultiAttack m : allAttack) {
+      m.prepare();
+    }
+    for (MultiAttack m : allAttack) {
+      m.perform();
+    }
+  }
+
+  public Map getMap() {
+    return map;
+  }
+}
---
+++ shared/src/test/java/edu/duke/ece651/teamX/shared/AttackProcessorTest.java
@@ -0,0 +1,43 @@
+package edu.duke.ece651.teamX.shared;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class AttackProcessorTest {
+  @Test
+  public void test_resovle() {
+    
+    ArrayList<AttackSender> allAttack = new ArrayList<AttackSender>();
+    Territory t1 = new Territory("duke", 100);
+    Territory t2 = new Territory("cary", 80);
+    Territory t3 = new Territory("rdu", 7);
+    Territory t4 = new Territory("bos", 11);
+    Player p1 = new Player("zhou", 20);
+    Player p2 = new Player("andre", 15);
+    Player p3 = new Player("an", 15);
+
+    Map map = new Map();
+    map.addTerritory(t1, p1);
+    map.addTerritory(t2, p2);
+    map.addTerritory(t3, p3);
+    map.addTerritory(t4, p3);
+
+    TextDisplayer displayer = new TextDisplayer(map);
+    System.out.println(displayer.display());
+
+    allAttack.add(new AttackSender(t1, t2, 30));
+    allAttack.add(new AttackSender(t2, t3, 50));
+    allAttack.add(new AttackSender(t3, t1, 5));
+    allAttack.add(new AttackSender(t4, t2, 6));
+
+    AttackProcessor ap = new AttackProcessor(allAttack, map);
+    ap.resovleAllAttack();
+    System.out.println(displayer.display());
+    
+
+  }
+
+}
