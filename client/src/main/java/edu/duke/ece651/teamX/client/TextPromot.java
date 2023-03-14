98ad16ac3d62e57a534796921af6920f11992d0d
Xinmeng <xc139@duke.edu>
2023-03-14 14:19:02 -0400

    added TextPromot display group

---
+++ client/src/main/java/edu/duke/ece651/teamX/client/TextPromot.java
@@ -0,0 +1,56 @@
+package edu.duke.ece651.teamX.client;
+import edu.duke.ece651.teamX.shared.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+public class TextPromot {
+  private final Player player;
+  private String displayTerrtory(Territory t) {
+    StringBuilder ans = new StringBuilder("");
+    ans.append(t.getName());
+    Iterator<Territory> iter = t.getNeighbours();
+    ans.append(" (next to: ");
+    ans.append(iter.next().getName());
+    while (iter.hasNext()) {
+      ans.append(", ");
+      ans.append(iter.next().getName());
+    }
+    ans.append(")");
+    return ans.toString();
+  }
+
+  private String displayTerrWithUnit(Territory t) {
+    StringBuilder ans = new StringBuilder("");
+    ans.append(displayTerrtory(t));
+    ans.append(" with ");
+    ans.append(t.getUnitsNumber());
+    ans.append(" units");
+    return ans.toString();
+  }
+
+  public TextPromot(Player p) { player = p; }
+  public String displayBegin() {
+    StringBuilder ans = new StringBuilder("");
+    ans.append("Welcome to the RISK game, Player " + player.getName() + "!\n");
+    ans.append(
+        "Please wait a few moments while we gather all players before we start...\n");
+    return ans.toString();
+  }
+  public String displayTerrGroup(HashMap<Integer, ArrayList<Territory> > free_groups) {
+    StringBuilder ans = new StringBuilder("");
+    ans.append("Please choose one territory group as your initial territories:\n");
+    ans.append("(type the corresponding number to indicate your choice, e.g. 1)\n\n");
+    ArrayList<Integer> sorted_group = new ArrayList<>(free_groups.keySet());
+    Collections.sort(sorted_group);
+    for (int i : sorted_group) {
+      ans.append("(" + i + ") Group " + i + ":\n");
+      for (Territory t : free_groups.get(i)) {
+        ans.append("   - ");
+        ans.append(displayTerrtory(t));
+        ans.append("\n");
+      }
+    }
+    return ans.toString();
+  }
+}
---
+++ client/src/test/java/edu/duke/ece651/teamX/client/TextPromotTest.java
@@ -0,0 +1,58 @@
+package edu.duke.ece651.teamX.client;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import edu.duke.ece651.teamX.shared.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+
+public class TextPromotTest {
+  private HashMap<Integer, ArrayList<Territory> > formGroup() {
+    ArrayList<Territory> terr_list1 = new ArrayList<Territory>();
+    ArrayList<Territory> terr_list2 = new ArrayList<Territory>();
+    Territory a = new Territory("A");
+    Territory b = new Territory("B");
+    Territory c = new Territory("C");
+    Territory d = new Territory("D");
+    assertEquals(true, a.addNeighbors(b));
+    assertEquals(true, a.addNeighbors(c));
+    assertEquals(true, b.addNeighbors(d));
+    assertEquals(true, c.addNeighbors(d));
+    terr_list1.add(a);
+    terr_list1.add(b);
+    terr_list2.add(c);
+    terr_list2.add(d);
+    HashMap<Integer, ArrayList<Territory> > res =
+        new HashMap<Integer, ArrayList<Territory> >();
+    res.put(0, terr_list1);
+    res.put(1, terr_list2);
+    return res;
+  }
+  @Test
+  public void test_Begin() {
+    Player p1 = new Player("A", 20);
+    TextPromot promot = new TextPromot(p1);
+    String expected =
+        "Welcome to the RISK game, Player A!\n"
+        + "Please wait a few moments while we gather all players before we start...\n";
+    assertEquals(expected, promot.displayBegin());
+  }
+  @Test
+  public void test_displayGroup() {
+    Player p1 = new Player("A", 20);
+    TextPromot promot = new TextPromot(p1);
+    HashMap<Integer, ArrayList<Territory> > groups = formGroup();
+    String expected = "Please choose one territory group as your initial territories:\n"
+                      +
+                      "(type the corresponding number to indicate your choice, e.g. 1)\n"
+                      + "\n"
+                      + "(0) Group 0:\n"
+                      + "   - A (next to: B, C)\n"
+                      + "   - B (next to: A, D)\n"
+                      + "(1) Group 1:\n"
+                      + "   - C (next to: A, D)\n"
+                      + "   - D (next to: B, C)\n";
+    assertEquals(expected, promot.displayTerrGroup(groups));
+  }
+}
