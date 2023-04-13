58d063350ece8ef74840dbf249644558cfd32bd5
Xinmeng <xc139@duke.edu>
2023-04-13 02:10:34 -0400

    can set units

--- client/src/main/java/edu/duke/ece651/teamX/client/controller/SelectGroupController.java
+++ client/src/main/java/edu/duke/ece651/teamX/client/controller/SelectGroupController.java
@@ -30,30 +30,33 @@ public class SelectGroupController implements Controller {
     @FXML
     Text resultText;
 
-    public SelectGroupController(Stage st, Socket cs, ArrayList<String> np,HashMap<Integer, ArrayList<Territory>> tsm) {
+    public SelectGroupController(Stage st, Socket cs, ArrayList<String> np,
+            HashMap<Integer, ArrayList<Territory>> tsm) {
         namePassword = np;
         clientSocket = cs;
         stage = st;
-        territories=tsm;
+        territories = tsm;
     }
 
     @FXML
     public void chooseGroup(ActionEvent ae) throws IOException, ClassNotFoundException {
         Object source = ae.getSource();
-        if (source instanceof Button) {
-            Button btn = (Button) source;
-           String terrName = btn.getId();
-           for(int i : territories.keySet()){
-            ArrayList<Territory> ts = territories.get(i);
-            for(Territory t:ts){
-                if(t.getName().equals(terrName)){
+
+        Button btn = (Button) source;
+        String terrName = btn.getId();
+        ArrayList<Territory> ts ;
+        for (int i : territories.keySet()) {
+            ts= territories.get(i);
+            for (Territory t : ts) {
+                if (t.getName().equals(terrName)) {
                     Communicate.sendInt(clientSocket, i);
+                    SetUnitController suc = new SetUnitController(stage, clientSocket, namePassword, ts, 20);
+                    GeneralScreen<SetUnitController> rs = new GeneralScreen<>(suc);
+                    break;
                 }
             }
-           }
-        } else {
-            throw new IllegalArgumentException("Invalid source " + source + " for ActionEvent");
         }
+        
     }
 
     public void displayGroups() {
@@ -63,10 +66,15 @@ public class SelectGroupController implements Controller {
             for (Territory t : ts) {
                 Button button = (Button) scene.lookup("#" + t.getName());
                 button.setStyle("-fx-background-color: " + colors[i] + ";");
-                button.setOnAction(event -> {try{chooseGroup(event);}catch(Exception e){}});
+                button.setOnAction(event -> {
+                    try {
+                        chooseGroup(event);
+                    } catch (Exception e) {
+                    }
+                });
             }
         }
-        
+
     }
 
     public void setNewLayout() throws IOException {
