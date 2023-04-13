73ef5f3dd664c22324819460bf78e815d4d44574
Xinmeng <xc139@duke.edu>
2023-04-13 00:12:39 -0400

    added display map and choose territory group

--- client/src/main/java/edu/duke/ece651/teamX/client/Client.java
+++ client/src/main/java/edu/duke/ece651/teamX/client/Client.java
@@ -1,6 +1,8 @@
 package edu.duke.ece651.teamX.client;
 
 import edu.duke.ece651.teamX.shared.*;
+import edu.duke.ece651.teamX.client.controller.*;
+import edu.duke.ece651.teamX.client.view.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -8,43 +10,53 @@ import java.net.Socket;
 import java.util.ArrayList;
 import javafx.util.Pair;
 import java.util.HashMap;
+import javafx.stage.Stage;
 
 public class Client {
 
   private Socket socket;
   private Communicate communicate;
   private Player player; // init
-  private TextPrompt prompt; // init
+  // private TextPrompt prompt; // init
   private Map map;
-  private PrintStream out;
-  private UserInReader inputReader;
+  private Stage stage;
+  // private PrintStream out;
+  // private UserInReader inputReader;
   private ArrayList<Player> losers;
   private int foodResource;
   private int techResource;
+  private ArrayList<String> namePassword;
 
   /**
    * Construct a Client object
    *
    * @param s is socket to communicate with server
    */
-  public Client(Socket s, BufferedReader input, PrintStream out) {
+  public Client(Socket s, Stage st, ArrayList<String> np) {
     socket = s;
+    stage = st;
     communicate = new Communicate();
     map = new Map();
-    this.out = out;
-    this.inputReader = new UserInReader(input, out);
+    namePassword = np;
   }
 
   /**
-   * Initialze the game settings for client Receive player to get player properties (name, num of
+   * Initialze the game settings for client Receive player to get player
+   * properties (name, num of
    * units) Choose a territory group to sart Set Units
    */
   public void init() throws IOException, ClassNotFoundException {
     player = communicate.receivePlayer(socket);
-    prompt = new TextPrompt(player);
-    out.print(prompt.startPrompt());
-    ArrayList<Territory> territories = chooseTerrGroup();
-    setAllUnits(territories); // place units in territories
+    HashMap<Integer, ArrayList<Territory>> ts = (HashMap<Integer, ArrayList<Territory>>) Communicate
+        .receiveObject(socket);  
+        SelectGroupController sgc = new SelectGroupController(stage, socket, namePassword, ts);
+        GeneralScreen<SelectGroupController> sgs = new GeneralScreen<>(sgc);
+        
+    // prompt = new TextPrompt(player);
+    // out.print(prompt.startPrompt());
+
+    // ArrayList<Territory> territories = chooseTerrGroup();
+    // setAllUnits(territories); // place units in territories
     // attacks = new ArrayList<AttackSender>();
     // moves = new ArrayList<MoveSender>();
   }
@@ -56,12 +68,14 @@ public class Client {
    * @throws IOException
    * @throws ClassNotFoundException
    */
-  public ArrayList<Territory> chooseTerrGroup() throws IOException, ClassNotFoundException {
-    ClientGroupSetting groupSetting = new ClientGroupSetting(socket, out, inputReader, prompt);
-    groupSetting.perform();
-    groupSetting.commit();
-    return groupSetting.getGroup();
-  }
+  // public ArrayList<Territory> chooseTerrGroup() throws IOException, ClassNotFoundException {
+  //   // ClientGroupSetting groupSetting = new ClientGroupSetting(socket, out,
+  //   // inputReader, prompt);
+  //   // groupSetting.perform();
+  //   // groupSetting.commit();
+  //   // return groupSetting.getGroup();
+
+  // }
 
   /**
    * Place all available units to the territories
@@ -69,13 +83,13 @@ public class Client {
    * @param territories is the group from chooseTerrGroup function
    * @throws IOException
    */
-  public void setAllUnits(ArrayList<Territory> territories) throws IOException {
-    ClientUnitSetting unitSetting = new ClientUnitSetting(socket, out, inputReader, prompt,
-        territories,
-        player.getUnitNum());
-    unitSetting.perform();
-    unitSetting.commit();
-  }
+  // public void setAllUnits(ArrayList<Territory> territories) throws IOException {
+  //   ClientUnitSetting unitSetting = new ClientUnitSetting(socket, out, inputReader, prompt,
+  //       territories,
+  //       player.getUnitNum());
+  //   unitSetting.perform();
+  //   unitSetting.commit();
+  // }
 
   /**
    * Receive map from server
@@ -89,7 +103,7 @@ public class Client {
    */
   public void displayMap() throws IOException {
     TextDisplayer dis = new TextDisplayer(map);
-    out.print(dis.display());
+    // out.print(dis.display());
   }
 
   /**
@@ -103,7 +117,6 @@ public class Client {
       act.commit();
     }
   }
-
 
   private GameResult receiveGameResult() throws IOException, ClassNotFoundException {
     return communicate.receiveGameResult(socket);
@@ -125,7 +138,7 @@ public class Client {
     foodResource = f;
   }
 
-  //ability to increment tech resources at the end of each turn
+  // ability to increment tech resources at the end of each turn
   public void AddTechResource(Map TerrMap, Pair<Integer, String> PlayerInfo) {
 
     TerritoryProduce AddResource = new TerritoryProduce();
@@ -139,7 +152,7 @@ public class Client {
     }
   }
 
-  //ability to increment food resources at the end of each turn
+  // ability to increment food resources at the end of each turn
   public void AddFoodResource(Map TerrMap, Pair<Integer, String> PlayerInfo) {
 
     TerritoryProduce AddResource = new TerritoryProduce();
@@ -153,38 +166,36 @@ public class Client {
     }
   }
 
-
   /**
    * Let client to player one turn
    *
    * @throws IOException
    */
   public void playOneTurn() throws IOException {
-    ClientAttack attack = new ClientAttack(socket, out, inputReader, prompt, map, player);
-    ClientMove move = new ClientMove(socket, out, inputReader, prompt, map, player);
-    while (true) {
-      displayMap();
-      out.print(prompt.oneTurnPrompt());
-      String user_in = inputReader.readString();
-      user_in = user_in.toUpperCase();
-      switch (user_in) {
-        case "M":
-          move.perform();
-          break;
-        case "A":
-          attack.perform();
-          break;
-        case "D":
-          performCommit(move, attack);
-          out.print(prompt.commitMessage());
-          return;
-        default:
-          out.print(prompt.enterAgainPrompt());
-          break;
-      }
-    }
+    // ClientAttack attack = new ClientAttack(socket, out, inputReader, prompt, map, player);
+    // ClientMove move = new ClientMove(socket, out, inputReader, prompt, map, player);
+    // while (true) {
+    //   displayMap();
+    //   out.print(prompt.oneTurnPrompt());
+    //   String user_in = inputReader.readString();
+    //   user_in = user_in.toUpperCase();
+    //   switch (user_in) {
+    //     case "M":
+    //       move.perform();
+    //       break;
+    //     case "A":
+    //       attack.perform();
+    //       break;
+    //     case "D":
+    //       performCommit(move, attack);
+    //       out.print(prompt.commitMessage());
+    //       return;
+    //     default:
+    //       out.print(prompt.enterAgainPrompt());
+    //       break;
+    //   }
+    // }
   }
-
 
   public void playTurns() throws IOException, ClassNotFoundException {
     while (true) {
@@ -193,12 +204,12 @@ public class Client {
       if (!gameResult.isWin() && !gameResult.loserContains(player)) {
         playOneTurn();
       } else if (gameResult.isWin()) {
-        out.println("Game finished!");
-        out.println("Winner is " + gameResult.getWinner().getName());
+        // out.println("Game finished!");
+        // out.println("Winner is " + gameResult.getWinner().getName());
         return;
       } else if (gameResult.loserContains(player)) {
         displayMap();
-        out.println("You lose! Right now you are watching the game!");
+        // out.println("You lose! Right now you are watching the game!");
       }
     }
   }
@@ -222,8 +233,8 @@ public class Client {
    */
   public void continuePlay() throws IOException, ClassNotFoundException {
     player = communicate.receivePlayer(socket);
-    prompt = new TextPrompt(player);
-    out.println("Welcome back, player " + player.getName());
+    // prompt = new TextPrompt(player);
+    // out.println("Welcome back, player " + player.getName());
     playTurns();
   }
 }
